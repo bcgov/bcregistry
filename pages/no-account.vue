deb0d1155ea87931d4337f283c8953072cdc8671
Doug Lovett <doug@diamante.ca>
2021-06-07 13:11:38 -0700

    BCROS site add starter content.
    Signed-off-by: Doug Lovett <doug@diamante.ca>

---
+++ pages/no-account.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container fluid no-gutters>
+    <div class="account-container">
+      <h1>No Account Required</h1>
+    </div>
+  </v-container>
+</template>
+
+<script>
+</script>
