8decb09cd29240dfb566f3cc0a2ae433dee0e7a3
deetz99 <dietrich@wolpert.ca>
2024-09-21 20:38:19 -0700

    add french i18n

--- app/components/Sbc/AuthMenu.vue
+++ app/components/Sbc/AuthMenu.vue
@@ -10,6 +10,7 @@ defineProps<{
     id="logged-out-options-dropdown"
     :items="connectNav.loggedOutUserOptions"
     :ui="{
+      width: 'min-w-fit',
       item: {
         base: 'group flex items-center gap-4 w-full',
         disabled: 'cursor-default opacity-100',
@@ -31,7 +32,7 @@ defineProps<{
     />
 
     <template #method>
-      <span class="font-semibold text-bcGovColor-darkGray"> {{ $t('label.selectLoginMethod') }} </span>
+      <span class="whitespace-nowrap font-semibold text-bcGovColor-darkGray"> {{ $t('label.selectLoginMethod') }} </span>
     </template>
   </UDropdown>
 </template>
