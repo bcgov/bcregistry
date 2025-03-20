13c022d762e3f22b75a0af2de6a4cdfabb463555
deetz99 <dietrich@wolpert.ca>
2025-03-19 17:38:19 -0700

    fix ld variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -149,7 +149,8 @@ export default defineNuxtConfig({
       myBusinessRegistryDashboard: `${process.env.NUXT_AUTH_WEB_URL}account/{accountId}/business`,
       pprDashboard: `${process.env.NUXT_PPR_URL}/dashboard`,
       strrURL: process.env.NUXT_STRR_URL,
-      createAccountNoAuthURL: `${process.env.NUXT_AUTH_WEB_URL}choose-authentication-method`
+      createAccountNoAuthURL: `${process.env.NUXT_AUTH_WEB_URL}choose-authentication-method`,
+      ldClientId: process.env.NUXT_LD_CLIENT_ID || '',
     }
   }
 })
