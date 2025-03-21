74254031b265e5c3934717d0bc04e355d8557cfa
deetz99 <dietrich@wolpert.ca>
2025-03-21 10:26:30 -0700

    use login from local keycloak, not relying on auth-web

--- app/middleware/redirect-pages.global.ts
+++ app/middleware/redirect-pages.global.ts
@@ -19,18 +19,24 @@ export default defineNuxtRouteMiddleware(async (to) => {
     const registryDashboardUrl = rtc.baseUrl + locale + '/dashboard'
     const logoutRedirectUrl = sessionStorage.getItem('LOGOUT_URL') || registryLoginUrl
     const loginUrl = sessionStorage.getItem('LOGIN_URL') || registryDashboardUrl
+    const { login, logout } = useKeycloak()
 
     switch (path) {
       case RedirectPaths.FILING:
         return navigateTo(rtc.supportFilingUrl, { external: true })
       case RedirectPaths.SIGNOUT:
-        return await useKeycloak().logout(logoutRedirectUrl)
-      case RedirectPaths.SIGNIN_BCEID: // TODO: Any reason we can't use useKeycloak().login(IdpHint.BCEID, encodeURIComponent(loginUrl)) here ?
-        return navigateTo(rtc.authWebURL + 'signin/bceid/' + encodeURIComponent(loginUrl), { external: true })
+        return await logout(logoutRedirectUrl)
+      case RedirectPaths.SIGNIN_BCEID:
+        return await login(IdpHint.BCEID, loginUrl)
       case RedirectPaths.SIGNIN_BCSC:
-        return navigateTo(rtc.authWebURL + 'signin/bcsc/' + encodeURIComponent(loginUrl), { external: true })
+        return await login(IdpHint.BCSC, loginUrl)
       case RedirectPaths.SIGNIN_IDIR:
-        return navigateTo(rtc.authWebURL + 'signin/idir/' + encodeURIComponent(loginUrl), { external: true })
+        return await login(IdpHint.IDIR, loginUrl)
     }
   }
 })
+
+// Leaving this here in case we need to switch back
+// return navigateTo(rtc.authWebURL + 'signin/bceid/' + encodeURIComponent(loginUrl), { external: true })
+// return navigateTo(rtc.authWebURL + 'signin/bcsc/' + encodeURIComponent(loginUrl), { external: true })
+// return navigateTo(rtc.authWebURL + 'signin/idir/' + encodeURIComponent(loginUrl), { external: true })
