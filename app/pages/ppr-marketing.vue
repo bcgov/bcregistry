7d5ceb20b692548a32ffea778739e40fa0ddd9e6
Cameron Bowler <cameron.bowler@ca.ey.com>
2025-08-26 08:48:21 -0700

    Update referred Legal Services label and link

--- app/pages/ppr-marketing.vue
+++ app/pages/ppr-marketing.vue
@@ -67,7 +67,7 @@ const helpfulLinks = [
     link: {
       label: t('page.ppr.section.helpfulLinks.cards.legal.link'),
       target: '_blank',
-      href: 'https://dyedurham.com/'
+      href: 'https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/citizens-services/servicebc'
     }
   }
 ]
